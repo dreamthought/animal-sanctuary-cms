ee7728d5a91b226fb4819aabda0bdec540b61a49
Raf Gemmail <rafiq@dreamthought.com>
2024-02-03 22:00:48 +0000

    nix - added dev env for nix-shell

--- default.nix
+++ default.nix
@@ -10,13 +10,20 @@
     gcc
     zsh
     vim
+    git
   ]);
   multiPkgs = pkgs: (with pkgs; [
   ]);
   runScript = "${run}";
   profile =
     '' 
+      export NVM_DIR="$HOME/.nvm"
       unset LD_LIBRARY_PATH
+      if [ ! -d "$NVM_DIR" ]; then
+        git clone https://github.com/nvm-sh/nvm.git ~/.nvm
+      fi
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     '';
 }).env
 
