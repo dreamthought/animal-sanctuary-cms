a960a642a4ca08798f36f486c452273d9fdd3dab
Byssa <name@email.com>
2024-02-11 18:08:27 +1300

    Added cowcare

---
+++ src/routes/cowcare/+page.svelte
@@ -0,0 +1,35 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Cowcare</title>
+</svelte:head>
+
+<!-- Content CowCare -->
+<div class="container custom-content shadow-lg">
+    <h1>COW CARE - a new option to save lives</h1>
+      <div>
+        <div class="custom-wrap-img text-center">
+          <img src="images/help/cow.jpg" class="rounded" alt="Young cows">
+        </div>
+        <p class="custom-wrap-text">
+        <b>Rescued cows finally have an option.  If we find them a sponsor, they can go to Cow Care to live out their full life.  
+        Cow Care provides all the facilities to care for cows for the rest of their lives: beautiful grazing on a huge property, 
+        they're moved to fresh grass every day, weighed regularly, checked on daily, given supplements, baylage in winter if necessary, annual vaccination, etc. etc. etc.</b>
+      </p>
+      <p class="custom-wrap-text">
+        The cost to sponsor a cow is $12 +GST per week.  That's less than some places charge just for grazing, with no individual care for the animals. 
+        As a sponsor you can visit your cow; receive reports, photos & updates about your cow; join our private FB sponsors page; 
+        receive invites to private Cow Care functions such as picnics, etc.
+      </p>
+      <p class="custom-wrap-text">
+        If you're interested, I'd love to chat with you to provide the full story, and answer any questions. 
+        Please <a href="contact" class="in-text-link">CONTACT US</a> with your name and phone number, and I'll give you a call. Thank you.  
+        (Note, if you're a private owner that wants to place your own cows at Cow Care, that's great!  Let me know that and I'll send you a flyer.)
+      </p>
+<hr/>
+      <div class="text-end">
+        <a class="btn btn-outline-success" href="help" role="button">Back to Donate/Help</a>
+      </div>
+
+      </div>
+  </div>
+  <!-- End Content CowCare -->
