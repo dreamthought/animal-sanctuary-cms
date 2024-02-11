0843db89dd8f8be465cb2ac91741a97bcc887a28
Byssa <name@email.com>
2024-02-11 17:29:34 +1300

    Updated animals content

--- src/routes/animals/+page.svelte
+++ src/routes/animals/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>The Animal Sanctuary</title>
+  <title>The Animal Sanctuary - Animals</title>
 </svelte:head>
 
 <script>
@@ -7,96 +7,96 @@
 
 <!-- Meet The Animals -->
 <div class="container custom-content shadow-lg">
-<h1>MEET THE ANIMALS</h1>
-<div class="container">
-  <p>
-    <b>Because there are a number of rescue agencies set up for dogs and cats, The Sanctuary’s primary focus is on other creatures in need of help… 
-    and in particular animals that have been ill-treated.</b>
-  </p>
-  <p>
-    The Sanctuary has so far provided a safe haven for a variety of donkeys, pigs, goats, battery hens, parrots, ducks, dogs, cats, a turtle and a turkey!
-  </p>
-  <p>
-    All of these animals were suffering from neglect or direct abuse, and desperately needed a good home with lots of tender loving care. 
-    Once these beautiful animals realize that they won’t be hurt any more, they become the most gentle and caring companions with amazing personalities. 
-    Click on the links below to meet some of our happy residents!
-  </p>
-</div>
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card custom-card-animals">
-          <h4 class="card-header custom-card-title-animals">Equines</h4>
-          <img src="images/animals/animals-1-equines.jpg" alt="equines" class="card-img-top" />
-          <div class="card-img-overlay">
-            <a href="equines.html" class="btn custom-btn-animals">Learn more</a>
-          </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card custom-card-animals">
-        <h4 class="card-header custom-card-title-animals">Goats</h4>
-        <img src="images/animals/animals-2-goats.jpg" alt="goats" class="card-img-top" />
-        <div class="card-img-overlay">
-          <a href="goats.html" class="btn custom-btn-animals">Learn more</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card custom-card-animals">
-          <h4 class="card-header custom-card-title-animals">Pigs</h4>
-          <img src="images/animals/animals-3-pigs.jpg" alt="pigs" class="card-img-top" />
-          <div class="card-img-overlay">
-            <a href="pigs.html" class="btn custom-btn-animals">Learn more</a>
-          </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card custom-card-animals">
-          <h4 class="card-header custom-card-title-animals">Parrots</h4>
-          <img src="images/animals/animals-4-parrots.jpg" alt="parrots" class="card-img-top" />
-          <div class="card-img-overlay">
-            <a href="parrots.html" class="btn custom-btn-animals">Learn more</a>
-          </div>
-        </div>
-    </div>
+  <h1>MEET THE ANIMALS</h1>
+  <div class="container">
+    <p>
+      <b>Because there are a number of rescue agencies set up for dogs and cats, The Sanctuary’s primary focus is on other creatures in need of help… 
+      and in particular animals that have been ill-treated.</b>
+    </p>
+    <p>
+      The Sanctuary has so far provided a safe haven for a variety of donkeys, pigs, goats, battery hens, parrots, ducks, dogs, cats, a turtle and a turkey!
+    </p>
+    <p>
+      All of these animals were suffering from neglect or direct abuse, and desperately needed a good home with lots of tender loving care. 
+      Once these beautiful animals realize that they won’t be hurt any more, they become the most gentle and caring companions with amazing personalities. 
+      Click on the links below to meet some of our happy residents!
+    </p>
   </div>
-  <div class="row">
+    <div class="row">
       <div class="col-md-3">
         <div class="card custom-card-animals">
-            <h4 class="card-header custom-card-title-animals">Ducks</h4>
-            <img src="images/animals/animals-5-ducks.jpg" alt="ducks" class="card-img-top" />
+            <h4 class="card-header custom-card-title-animals">Equines</h4>
+            <img src="images/animals/animals-1-equines.jpg" alt="equines" class="card-img-top" />
             <div class="card-img-overlay">
-              <a href="ducks.html" class="btn custom-btn-animals">Learn more</a>
+              <a href="equines.html" class="btn custom-btn-animals">Learn more</a>
             </div>
           </div>
       </div>
       <div class="col-md-3">
         <div class="card custom-card-animals">
-          <h4 class="card-header custom-card-title-animals">Peacocks</h4>
-          <img src="images/animals/animals-6-peacocks.jpg" alt="peacocks" class="card-img-top" />
-           <div class="card-img-overlay">
-            <a href="peacocks.html" class="btn custom-btn-animals">Learn more</a>
+          <h4 class="card-header custom-card-title-animals">Goats</h4>
+          <img src="images/animals/animals-2-goats.jpg" alt="goats" class="card-img-top" />
+          <div class="card-img-overlay">
+            <a href="goats.html" class="btn custom-btn-animals">Learn more</a>
           </div>
         </div>
       </div>
       <div class="col-md-3">
         <div class="card custom-card-animals">
-            <h4 class="card-header custom-card-title-animals">Hens</h4>
-            <img src="images/animals/animals-7-hens.jpg" alt="hens" class="card-img-top" />
+            <h4 class="card-header custom-card-title-animals">Pigs</h4>
+            <img src="images/animals/animals-3-pigs.jpg" alt="pigs" class="card-img-top" />
             <div class="card-img-overlay">
-              <a href="ourhens.html" class="btn custom-btn-animals">Learn more</a>
+              <a href="pigs.html" class="btn custom-btn-animals">Learn more</a>
             </div>
           </div>
       </div>
       <div class="col-md-3">
         <div class="card custom-card-animals">
-            <h4 class="card-header custom-card-title-animals">Native Birds</h4>
-            <img src="images/animals/animals-8-nativebirds.jpg" alt="native birds" class="card-img-top" />
+            <h4 class="card-header custom-card-title-animals">Parrots</h4>
+            <img src="images/animals/animals-4-parrots.jpg" alt="parrots" class="card-img-top" />
             <div class="card-img-overlay">
-              <a href="nativebirds.html" class="btn custom-btn-animals">Learn more</a>
+              <a href="parrots.html" class="btn custom-btn-animals">Learn more</a>
             </div>
           </div>
       </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-md-3">
+          <div class="card custom-card-animals">
+              <h4 class="card-header custom-card-title-animals">Ducks</h4>
+              <img src="images/animals/animals-5-ducks.jpg" alt="ducks" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="ducks.html" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-animals">
+            <h4 class="card-header custom-card-title-animals">Peacocks</h4>
+            <img src="images/animals/animals-6-peacocks.jpg" alt="peacocks" class="card-img-top" />
+             <div class="card-img-overlay">
+              <a href="peacocks.html" class="btn custom-btn-animals">Learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-animals">
+              <h4 class="card-header custom-card-title-animals">Hens</h4>
+              <img src="images/animals/animals-7-hens.jpg" alt="hens" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="ourhens.html" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-animals">
+              <h4 class="card-header custom-card-title-animals">Native Birds</h4>
+              <img src="images/animals/animals-8-nativebirds.jpg" alt="native birds" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="nativebirds.html" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+      </div>
+  </div>
 <!-- End Meet The Animals -->
