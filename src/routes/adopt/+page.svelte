f09ae0ab03c33904a5f39ab5a2570d9a4fd90786
Byssa <name@email.com>
2024-02-11 17:24:51 +1300

    Added adopt content

---
+++ src/routes/adopt/+page.svelte
@@ -0,0 +1,28 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Adopt</title>
+</svelte:head> 
+ 
+ <!-- Adopt -->
+ <div class="container custom-content shadow-lg">
+    <h1>AVAILABLE FOR ADOPTION</h1>
+      <div class="container">
+      <h2>Looking for Love!</h2>
+        <p>
+        <b>While some animals come to the Sanctuary for the rest of their lives, others would be happy to find new homes with loving families all to themselves.
+        If you are interested in any of the animals below, please contact us for more information about them and the adoption process.</b>
+      </p>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card custom-card-animals">
+          <h4 class="card-header custom-card-title-animals">Ex-Battery hens</h4>
+          <img src="images/adopt/hen.jpg" alt="hens" class="card-img-top" />
+          <div class="card-img-overlay">
+            <a href="hens.html" class="btn custom-btn-animals">Learn more</a>
+          </div>
+        </div>
+      </div>
+      </div>
+  </div>
+  <!-- End Adopt -->
