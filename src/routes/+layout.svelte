745c393a42be3388c23b5a7701765d2db46e6008
Byssa <name@email.com>
2024-02-11 22:37:11 +1300

    Fixed Animal Sanctuary small text in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
      alt="Animal Sanctuary Banner" class="img-fluid" />
     </a>
     <p>
-    The Sanctuary is an animal refuge run by Shawn Bishop and Michael Dixon in Matakana, New Zealand. We provide a safe haven for abused and neglected animals, 
+    The Animal Sanctuary is an animal refuge run by Shawn Bishop and Michael Dixon in Matakana, New Zealand. We provide a safe haven for abused and neglected animals, 
     and care for injured or orphaned native birds. A charitable trust has been established for this purpose.
     </p>
     <span><i class="fa fa-copyright"></i> 2024 The Animal Sanctuary</span>
