61a8821e343700558d17c28403937f454506fda9
Byssa <name@email.com>
2024-02-11 18:44:12 +1300

    Added Peacocks

---
+++ src/routes/peacocks/+page.svelte
@@ -0,0 +1,67 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Animals - Peacocks</title>
+</svelte:head>
+
+<!-- Content Peacocks -->
+<div class="container custom-content shadow-lg">
+    <h1>PEACOCKS</h1>
+    <hr>
+    <h2>Prince Charles</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/peacocks-prince-charles.jpg" class="rounded img-fluid" alt="Solomon the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Charles and his brothers were rescued by the Sanctuary from a farmer that was selling his property and had decided to shoot the peacocks. 
+          We immediately offered to take four, and were shocked to receive them tied up in brown canvas sacks with only their heads poking out.
+        </p>
+        <p>
+          We set them free around the property, and you can now see them poking through the bush or jumping off the roof, 
+          flapping their wings and huge tails behind them. Sadly, our fourth peacock passed away recently, so Charles spends his days wandering alone, 
+          looking in on the chickens and ducks and sunning himself wherever there is long grass.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Prince William</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/peacocks-prince-william.jpg" class="rounded img-fluid" alt="Bartholomew the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Peacocks are the national bird of India, and were once only owned by Indian maharajas, so we have given our peacocks royal names as well. 
+          William, as the heir to the throne, is the most noble of the three, with a full, long set of tail feathers that he raises in full display, 
+          even though we don’t have any peahens for him to impress.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Prince Harry</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/peacocks-prince-harry.jpg" class="rounded img-fluid" alt="Romeo the miniature pony">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Harry is the inquisitive one out of our three peacocks, and spends most of his time hanging around in the backyard, 
+          looking through the windows into the house to see if anyone is around. He is also close friends with William, 
+          and the two of them eat, sleep and cause mischief together – as young boys will do.
+        </p>
+      </div>
+    </div>
+<hr/>
+    <div class="text-end">
+      <a class="btn btn-outline-success" href="animals" role="button">Back to Animals</a>
+    </div>
+
+  </div>
+  <!-- End Content Peacocks -->
