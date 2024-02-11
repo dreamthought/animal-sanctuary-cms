aa234d3b285344ed988e3366a59fb22ad8fd8dfc
Byssa <name@email.com>
2024-02-11 18:51:34 +1300

    Added Support

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,64 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Support</title>
+</svelte:head>
+
+<!-- Content Supporter -->
+<div class="container custom-content shadow-lg">
+    <h1>SANCTUARY SUPPORTERS</h1>
+      <div class="container">
+      <h2><i>Would you spare a coffee for the animals?</i></h2>
+        <p>
+        <b>For the price of one good coffee a month, you can provide real help to injured and orphaned native birds and rescued abused animals.</b>
+      </p>
+      <p>
+        Become a Sanctuary Supporter with an automatic deduction of $5 or more per month. 
+        You will be invited to exclusive Supporters events, can cuddle the animals and visit the birds, and will receive regular photos and updates. 
+        And because we are a registered charity, you’ll even receive one-third of your donation each year back from the government. 
+        Which means you can join for LESS than a cup of coffee each month!
+      </p>
+      <h3>HOW TO BECOME A SUPPORTER</h3>
+      <p>
+        1. Go to online banking or to your nearest bank branch.
+      </p>
+      <p>
+        2. Set up an Automatic Payment of $5 per month or more, to:
+        <br>Account Name: The Animal Sanctuary Charitable Trust
+,         <br>Account Number: 12-3026-0327945-00
+M         <br>make sure your name is in the payment code
+      </p>
+      <p>
+        3. Now that you have signed up for a donation, please fill out the following form with your details so we know the donations are from you, 
+        and we can send you updates, invitations, and your tax deductible receipt.
+      </p>
+      <h2><i class="fa fa-heart" aria-hidden="true"></i> I am now an Animal Sanctuary Supporter <i class="fa fa-heart" aria-hidden="true"></i></h2>
+    </div>
+    <!-- Supporters Form -->
+    <form action="#">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="name" class="form-control" id="name" placeholder="Please enter your name" name="name">
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Please enter your email" name="email">
+      </div>
+      <div class="form-group">
+        <label for="address" class="form-label">Address:</label>
+        <textarea type="address" class="form-control" id="address" rows="4" placeholder="Please enter your full postal address"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="payment">Payment Code:</label>
+        <input type="payment" class="form-control" id="payment" placeholder="Please enter your payment code" name="payment">
+      </div>
+
+      <button type="submit" class="btn btn-success d-flex mx-auto mt-3">Send</button>
+    </form> 
+    <!-- Supporters Form -->
+<hr/>
+    <div class="text-end">
+      <a class="btn btn-outline-success" href="help" role="button">Back to Donate/Help</a>
+    </div>
+
+  </div>
+  <!-- End Content Supporter -->
