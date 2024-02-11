df93f4ad627954393e031824186574c979de1a22
Byssa <name@email.com>
2024-02-11 18:39:42 +1300

    Added OurHens

---
+++ src/routes/ourhens/+page.svelte
@@ -0,0 +1,114 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Our Hens</title>
+</svelte:head>
+
+<!-- Content Hens -->
+<div class="container custom-content shadow-lg">
+    <h1>HENS</h1>
+    <hr>
+    <h2>Where are they now?</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/hens-where.jpg" class="rounded img-fluid" alt="Solomon the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          I love getting photos and stories from people who’ve adopted from us. Kathy R sent us this photo, with the comment 
+          “Happy little rescue hens and their new friends, plus Russell the Rooster keeping an eye on everything.” 
+          Isn’t it wonderful to see them in their new home, enjoying the sun and the good life? I’ve been told that alpacas love hens. 
+          Looks like it’s true! Thanks Kathy.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Snuggles</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/hens-snuggles.jpg" class="rounded img-fluid" alt="Bartholomew the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Snuggles was rescued from a battery farm where the tip of her beak was cut off, so she is now unable to pick up food normally. 
+          Because battery hens are in such confined conditions, they can attack and cannibalize each other from fear, stress and frustration, 
+          and some feel that this harsh treatment is the only solution. But once the hens are free to roam and express their natural behaviour, 
+          they are never violent, and are actually quite gentle and loving.
+        </p>
+        <p>
+          Like her name suggests, Snuggles loves attention, and is the first hen to greet you for the evening feed. 
+          In the mornings she gets chicken mash, mixed with grit and water. Because she will lay protein-rich eggs, 
+          she also gets dark, leafy organic greens added to her diet, such as silver beet, full of vitamins, minerals and iron.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>My Little Friend</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/hens-mylittlefriend.jpg" class="rounded img-fluid" alt="Romeo the miniature pony">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          My Little Friend also had her beak trimmed, and therefore can’t pick up scattered chicken pellets. 
+          The uneven top and bottom beak means that she can’t grasp anything that is not at the back of her mouth. 
+          Both Snuggles and My Little Friend get fed from a special bowl that is deep enough so that they can eat, 
+          and it is quite cute to watch them both sharing their meal in the barn.
+        </p>
+        <p>
+          You can see that My Little Friend wears a blue leg band, and Snuggles pink. 
+          This is so that we can find our special girls quickly in the flock, and feed them separately. 
+          But over the years they have learned pretty quickly, and now they walk right over to their bowl 
+          and wait for their meal while the other hens are busy with their food.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Grandma</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/hens-grandma.jpg" class="rounded img-fluid" alt="Romeo the miniature pony">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Grandma is our oldest hen, and despite her age, she still runs around throughout the day, digging up worms and scratching in the grass for seeds. 
+          Hens live up to 8-10 years of age, but in battery farms they are slaughtered at 18 months because their egg production temporarily decreases. 
+          Thankfully Grandma has had a chance to live well into her twilight years here at the Sanctuary!          </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Tofu</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/hens-tofu.jpg" class="rounded img-fluid" alt="Romeo the miniature pony">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Tofu is not a chicken, but she certainly thinks she is. Everyday you can find our turkey sitting in the nesting box on some hen’s eggs, 
+          hoping they will hatch. When she first came to the sanctuary, she was so little the other hens bullied her, 
+          but she quickly outgrew them and now has earned top billing in the pecking order.
+        </p>
+        <p>
+          Other than sitting on eggs, Tofu enjoys living the life of a free range turkey that includes scratching, 
+          wandering around the sanctuary and just spending time with her hen friends.
+        </p>
+      </div>
+    </div>
+<hr/>
+    <div class="text-end">
+      <a class="btn btn-outline-success" href="animals" role="button">Back to Animals</a>
+    </div>
+
+  </div>
+  <!-- End Content Hens -->
+
+
