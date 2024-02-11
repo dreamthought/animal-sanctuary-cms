def21223d01ee66186e7c24f9a4e7ce3c4664ae3
Byssa <name@email.com>
2024-02-11 18:24:09 +1300

    Added gallery

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,99 @@
+<!-- Add headers here -->
+<svelte:head>
+	<title>The Animal Sanctuary - Gallery</title>
+</svelte:head>
+
+<!-- Gallery -->
+<div class="container custom-content shadow-lg">
+	<h1>GALLERY</h1>
+	<div class="row">
+		<!-- 1 of 4 image columns -->
+		<div class="col-lg-3 mb-4 mb-lg-0">
+			<img src="images/gallery/1-piglets.jpg" class="w-100 shadow rounded mb-4" alt="Piglets" />
+			<img src="images/gallery/5-pigeon.jpg" class="w-100 shadow rounded mb-4" alt="Pigeon" />
+			<img src="images/gallery/9-carell.jpg" class="w-100 shadow rounded mb-4" alt="Chickens" />
+			<img
+				src="images/gallery/13-penguin.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Wintry Mountain Landscape"
+			/>
+			<img
+				src="images/gallery/17-nellie-when-young.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Nellie the goat kid when young"
+			/>
+			<img
+				src="images/gallery/22-aracanas.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Orphans growing up"
+			/>
+		</div>
+		<!-- 2 of 4 image columns -->
+		<div class="col-lg-3 mb-4 mb-lg-0">
+			<img
+				src="images/gallery/21-nicola-kawana-with-donkeys.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Woman with donkeys"
+			/>
+			<img src="images/gallery/2-piglets.jpg " class="w-100 shadow rounded mb-4" alt="Piglets" />
+			<img src="images/gallery/6-chicken.jpg" class="w-100 shadow rounded mb-4" alt="Chicken" />
+			<img src="images/gallery/10-carell.jpg" class="w-100 shadow rounded mb-4" alt="Tree fern" />
+			<img src="images/gallery/14-pigs.jpg" class="w-100 shadow rounded mb-4" alt="Pigs" />
+			<img src="images/gallery/18-tui-freed.jpg" class="w-100 shadow rounded mb-4" alt="Tui bird" />
+		</div>
+		<!-- 3 of 4 image columns -->
+		<div class="col-lg-3 mb-4 mb-lg-0">
+			<img
+				src="images/gallery/3-dick-dibbles.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Ducks Dick and Dibbles"
+			/>
+			<img
+				src="images/gallery/7-chooks-turkey.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Chooks and a Turkey"
+			/>
+			<img
+				src="images/gallery/11-carell.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Pigs in barn"
+			/>
+			<img
+				src="images/gallery/15-orphans-growing-up.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Orphans growing up"
+			/>
+			<img src="images/gallery/24-carell.jpg" class="w-100 shadow rounded mb-4" alt="Daisies" />
+			<img
+				src="images/gallery/19-nicola-kawana-meets-a-penguin.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Woman holding a little blue penguin"
+			/>
+		</div>
+		<!-- 4 of 4 image columns -->
+		<div class="col-lg-3 mb-4 mb-lg-0">
+			<img
+				src="images/gallery/4-doris-with-pal-tipu.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Waves at Sea"
+			/>
+			<img src="images/gallery/8-carell.jpg" class="w-100 shadow rounded mb-4" alt="Flowers" />
+			<img
+				src="images/gallery/16-double-trouble.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Double Trouble - two owls"
+			/>
+			<img
+				src="images/gallery/12-carell.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Cabbage Trees"
+			/>
+			<img
+				src="images/gallery/20-lori-loves-apple-juice.jpg"
+				class="w-100 shadow rounded mb-4"
+				alt="Lorikeet drinking apple juice"
+			/>
+		</div>
+	</div>
+</div>
+<!-- End Gallery -->
