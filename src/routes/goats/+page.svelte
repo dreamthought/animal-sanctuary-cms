8090823db4bf57f52e0db531fbe275f60f37b851
Byssa <name@email.com>
2024-02-11 18:26:28 +1300

    Added Goats

---
+++ src/routes/goats/+page.svelte
@@ -0,0 +1,62 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - About</title>
+</svelte:head>
+
+<!-- Content Goats -->
+<div class="container custom-content shadow-lg">
+    <h1>GOATS</h1>
+    <hr>
+    <h2>Nellie</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/goats-nellie.jpg" class="rounded img-fluid" alt="Solomon the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Nellie was captured as a baby when a hunter shot her mother. Staked outside without a collar, the metal chain had worn into her neck. 
+          She had eaten everything within her small circle and was starving, and had no water in the hot summer sun. 
+          Her tiny bones stuck out, and she was suffering from worms and hoof-rot. The owner agreed to turn the small goat over to The Sanctuary.
+        </p>
+        <p>
+          Shawn says “Nellie was absolutely petrified, and I sat with her in a stable for several days without moving, without showing her my hands. 
+          I gradually moved food closer and closer to me, and kept speaking softly. I promised her she’d never be chained again. 
+          When I was finally able to release her into a paddock, Nellie looked confused and didn’t move at first. She’d forgotten what freedom was.
+        </p>
+        <p>
+          Then suddenly she starting racing around in huge circles, leaping and kicking with joy. 
+          Just as I was wondering if I’d ever get near her again she came back, braced her bottom against my leg, and started grazing. 
+          She’s been a beloved companion ever since.”
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Lucky</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/goats-lucky.jpg" class="rounded img-fluid" alt="Bartholomew the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Lucky was originally rescued from the abattoir, where he was in line for slaughter. 
+          He was brought to friends who wanted a goat for “lawn mowing”, and he performed this function well for a few years. 
+          However, the grounds are now landscaped and Lucky was no longer needed. He was terribly lonely ( goats have a strong herd instinct and HATE being alone), 
+          so Lucky was constantly breaking whatever chain or cable he was on and jumping the fence to be nearer to people.
+        </p>
+        <p>
+          His family asked if we would please give him a permanent home, and we agreed. Lucky is now happily settling in with our white goat Nellie, 
+          and they make a perfect pair. He runs around in the paddocks and enjoys his freedom – and the company.
+        </p>
+      </div>
+    </div>
+<hr/>
+    <div class="text-end">
+      <a class="btn btn-outline-success" href="animals" role="button">Back to Animals</a>
+    </div>
+    
+  </div>
+  <!-- End Content Goats -->
