77456267df47c8d9f7c5e4103eaa216a4efd2d77
Byssa <name@email.com>
2024-02-11 18:37:42 +1300

    Added Nativebirds

---
+++ src/routes/nativebirds/+page.svelte
@@ -0,0 +1,45 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Native Birds</title>
+</svelte:head>
+
+<!-- Native Bird Rehabilitation -->
+<div class="container custom-content shadow-lg">
+    <h1>NATIVE BIRD REHABILITATION</h1>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="text-center mb-2">
+          <img src="images/animals/nativebirds-1.jpg" class="rounded img-fluid" alt="baby chicks">
+        </div>
+        <div class="text-center mb-2">
+          <img src="images/animals/nativebirds-2.jpg" class="rounded img-fluid" alt="native wood pigeons">
+        </div>
+        <div class="text-center mb-2">
+          <img src="images/animals/nativebirds-3.jpg" class="rounded img-fluid" alt="little blue penguin">
+        </div>
+      </div>
+      <div class="col-md-8">
+        <p>
+          <b>The Sanctuary is situated on 32 acres of organic property with lots of native bush, and is bordered by 400 acres of protected bush reserve. 
+          This makes it a perfect bird site, so we are actively involved with the care of native birds such as tuis, wood pigeons, herons, moreporks, paradise ducks, etc.</b>
+        </p>
+        <p>
+          We often get baby and juvenile birds that need mothering before they can be released. Each bird receives a specialized baby diet that at first 
+          is given on the hour, progressing up to three times a day as they mature. Once they are eating on their own, 
+          they are moved into the recovery aviaries where they begin flying and learning how to recognize native foods. 
+          When they have the basics down, they are promoted to the larger flight aviary to build up their strength for release.
+        </p>
+        <p>
+          Injured birds are first seen by a veterinarian for xrays and assessment. Birds requiring daily medication and individual care are kept close by 
+          for regular treatement. Once stabilised, birds are placed in larger recovery aviaries to regain balance and movement. 
+          The final step in their rehab program is learning to fly vertically and for longer stretches in the big aviary, which is necessary for a successful release.
+        </p>
+        <p>
+          Our goal is the eventual release of all native birds in our care, and we support feed after release until the birds have successfully established 
+          their new life in the wild. You can be here to see the release yourself by becoming a Native Bird Buddy and supporting The Sanctuary with a regular donation. 
+          For more information, <a href="support" class="in-text-link">click here</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- End Native Bird Rehabilitation -->
