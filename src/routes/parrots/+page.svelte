5e30f7ae5b1082e4f78b3262e0e55ad9bd3db0ff
Byssa <name@email.com>
2024-02-11 18:41:50 +1300

    Added Parrots

---
+++ src/routes/parrots/+page.svelte
@@ -0,0 +1,57 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Animals - Parrots</title>
+</svelte:head>
+
+<!-- Parrots -->
+<div class="container custom-content shadow-lg">
+    <h1>PARROTS</h1>
+    <hr>
+    <h2>Rouge</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/parrots-rouge.jpg" class="rounded img-fluid" alt="Solomon the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Rouge is an Alexandrine parrot who was abandoned, and now lives at The Sanctuary. 
+          Rouge was not hand-raised, so when she first came to The Sanctuary she was a notorious biter, and with that big strong beak, she attacked everything. 
+          But like most of our animal residents Rouge just needed some time, love, and affection. 
+          She will now call out for a hop on your shoulder, and is happy to sit with you while you read or eat, sometimes requesting something to chew on.
+        </p>
+        <p>
+          Rouge is very dexterous, and unlike the other parrots, she can hold things much larger, or smaller than her. 
+          It is quite impressive to see with what agility and detail she can use her claws to open nuts, tear up sticks, or rip apart toys. 
+          Her favorite snack is corn on the cob, which she picks apart quite deftly and happily.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <h2>Max</h2>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="text-center mb-2">
+          <img src="images/animals/parrots-max.jpg" class="rounded img-fluid" alt="Bartholomew the donkey">
+        </div>
+      </div>
+      <div class="col-md-9">
+        <p>
+          Max is also an Alexandrine Parrot, and has become Rouge’s best friend. 
+          He is a complete sweetheart, and the first thing he looks for in the morning is a hop on your shoulder so he can spend the day with you. 
+          He is happy to step out onto your hand, and certainly enjoys a snuggle and a pat.
+        </p>
+        <p>
+          Max enjoys spending sunny days in the outdoor aviary with Rouge. Parrots bond with a mate, and these two have certainly found each other. 
+          Max’s favorite treat are sunflower seeds, which he opens up with his tongue and beak, no claws needed.
+        </p>
+      </div>
+    </div>
+<hr/>
+    <div class="text-end">
+      <a class="btn btn-outline-success" href="animals" role="button">Back to Animals</a>
+    </div>
+    
+  </div>
+  <!-- End Parrots -->
