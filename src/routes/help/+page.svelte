bf07c702d408543096f3c55e30c65bcbdecbce6c
Byssa <name@email.com>
2024-02-11 18:28:27 +1300

    Added help for Donate/Help

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,48 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Help/Donate</title>
+</svelte:head>
+
+<!-- Donate/Help -->
+<div class="container custom-content shadow-lg">
+    <h1>HOW YOU CAN HELP</h1>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card custom-card-help">
+              <h4 class="card-header custom-card-title-animals">Give a Donation</h4>
+              <img src="images/help/donate.jpg" alt="Make a donation" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="donate" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-help">
+            <h4 class="card-header custom-card-title-animals">Become a Supporter</h4>
+            <img src="images/help/support.jpg" alt="Become a supporter" class="card-img-top" />
+            <div class="card-img-overlay">
+              <a href="support" class="btn custom-btn-animals">Learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-help">
+              <h4 class="card-header custom-card-title-animals">Adopt a Hen</h4>
+              <img src="images/help/hen.jpg" alt="pigs" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="hens" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card custom-card-help">
+              <h4 class="card-header custom-card-title-animals">Save a Cow</h4>
+              <img src="images/help/cow.jpg" alt="parrots" class="card-img-top" />
+              <div class="card-img-overlay">
+                <a href="cowcare" class="btn custom-btn-animals">Learn more</a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  <!-- End Donate/Help -->
