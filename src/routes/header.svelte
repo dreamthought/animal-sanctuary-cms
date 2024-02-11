7030a942c92ab514ad4397c630496f08f7c88b75
Byssa <name@email.com>
2024-02-12 12:31:36 +1300

    Tried to fix lack of collapse on smaller screens. Was a local issue, works on deployed cms

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -12,8 +12,8 @@
   <!-- Navigation -->
   <nav class="sticky-top navbar navbar-expand-xl custom-navbar">
     <!-- svelte-ignore a11y-missing-content a11y-missing-attribute -->
-    <a class="d-lx-node d-xl-none navbar-brand" href="."></a>
-    <button class="navbar-toggler custom-toggler shadow-none text-end" type="button" data-bs-toggle="collapse"
+    <a class="d-lg-node d-xl-none navbar-brand" href="."></a>
+    <button class="navbar-toggler custom-toggler shadow-none" type="button" data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
       aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
