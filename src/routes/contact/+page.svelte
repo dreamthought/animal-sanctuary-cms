b2d4e8b5f70b80fae0cd98f9ddebf99468fff4c4
Byssa <name@email.com>
2024-02-11 17:51:18 +1300

    Updated Contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,11 @@
 <!-- Contact Us -->
 <div class="container custom-content shadow-lg">
-  
-  <!-- Contact and Form section -->
+  <!-- Form and Contact Details section -->
   <h1>CONTACT US</h1>
+  
   <div class="row">
-
+    <!-- Form -->
     <div class="col-md-6">
-      <!-- Contact Us Form -->
       <form action="#">
         <div class="form-group">
           <label for="name">Name:</label>
@@ -24,14 +23,14 @@
           <label for="message" class="form-label">Message:</label>
           <textarea type="message" class="form-control" id="message" rows="4" placeholder="Please type your message here"></textarea>
         </div>
-        <button type="submit" class="btn btn-success d-flex mx-auto mt-3">Submit</button>
+        <button type="submit" class="btn btn-success d-flex mx-auto mt-3">Send</button>
       </form> 
-      <!-- End Contact Us Form -->
     </div>
-
+    <!-- End Form -->
+    <!-- Contact Details -->
     <div class="col-md-6">
       <p><b>Please use the contact details below or fill out the form to get in contact with us. We look forward to hearing from you!</b></p>
-      <h4>Address:</h4>
+      <h4>Address</h4>
       <p>
         The Sanctuary
         <br>441 Govan Wilson Rd
@@ -39,22 +38,21 @@
         <br>RD5, Warkworth
         <br>New Zealand
       </p>
-      <h4>Phone:</h4>
+      <h4>Phone</h4>
       <p>09 422 7322</p>
       <h4>Email</h4>
       <p>animalsanctuary@xtra.co.nz</p>
     </div>
-    
+    <!-- End Contact Details -->     
   </div>
-  <!-- End Contact and Form section -->
 
-  <hr>
+<hr/>
 
   <!-- Google Map -->
   <div class="container text-center">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d102879.19689031561!2d174.57150233201511!3d-36.31291544976335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0cc32c12608d4f%3A0xae3c43623189cbde!2s441%20Govan%20Wilson%20Road%2C%20Whangaripo%200985!5e0!3m2!1sen!2snz!4v1706850963693!5m2!1sen!2snz" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d102879.19689031561!2d174.57150233201511!3d-36.31291544976335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0cc32c12608d4f%3A0xae3c43623189cbde!2s441%20Govan%20Wilson%20Road%2C%20Whangaripo%200985!5e0!3m2!1sen!2snz!4v1706850963693!5m2!1sen!2snz" width="100%" height="450" style="border:0;" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Google area map"></iframe>
   </div>
   <!-- End Google Map -->
-
+  
 </div>
-<!-- End Home -->
+<!-- End Form and Contact Details -->
