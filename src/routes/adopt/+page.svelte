813a27f08ba78a293e39c8d6deb51673be850131
Byssa <name@email.com>
2024-02-18 18:28:44 +1300

    Updated ADOPT cards to work on mobile

--- src/routes/adopt/+page.svelte
+++ src/routes/adopt/+page.svelte
@@ -3,7 +3,7 @@
 <title>The Animal Sanctuary - Adopt</title>
 </svelte:head> 
  
-<!-- Hens -->
+<!-- Adopt -->
 <div class="container custom-content shadow-lg">
   <h1>ADOPT</h1>
   <div class="container">
@@ -12,33 +12,64 @@
       <b>While some animals come to the Sanctuary for the rest of their lives, others would be happy to find new homes with loving families all to themselves.</b>
     </p>
   </div>
+  <!-- Cards on xl, lg and md screens -->
+  <div class="d-none d-lg-block">
   <div class="row">
     <div class="col-md-6">
-        <div class="card custom-card-adopt">
-            <img src=" images/adopt/hen.jpg" alt="A healthy adopted ex-egg farm hen" class="card-img" />
-            <div class="card-img-overlay">
-              <div class="card-body custom-card-body-adopt">
-                <h4 class="card-title custom-card-title-adopt">Egg Farm Hens</h4>
-                <p class="card-text custom-card-text-adopt">The Animal Sanctuary works closely with other sanctuaries to rescue, rehabilitate and re-home young hens from commercial egg farms.</p>
-                <a href="#hen" class="btn custom-btn-adopt">Learn more</a>
-              </div>
-            </div>
+      <div class="card custom-card-adopt">
+        <img src=" images/adopt/hen.jpg" alt="A healthy adopted ex-egg farm hen" class="card-img" />
+        <div class="card-img-overlay">
+          <div class="card-body custom-card-body-adopt">
+            <h4 class="card-title custom-card-title-adopt">Egg Farm Hens</h4>
+            <p class="card-text custom-card-text-adopt">The Animal Sanctuary works closely with other sanctuaries to rescue, rehabilitate and re-home young hens from commercial egg farms.</p>
+            <a href="#hen" class="btn custom-btn-adopt">Learn more</a>
           </div>
+        </div>
+      </div>
     </div>
     <div class="col-md-6">
-        <div class="card custom-card-adopt">
-            <img src="images/adopt/sheep.png" alt="Sheep in a green paddock" class="card-img" />
-            <div class="card-img-overlay">
-              <div class="card-body custom-card-body-adopt">
-                <h4 class="card-title custom-card-title-adopt">Other Animals</h4>
-                <p class="card-text custom-card-text-adopt">Other animals that are up for adoption are posted on The Animal Sanctuary Facebook page as they become available.  Please follow us to see who needs a new home…</p>
-                <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="btn custom-btn-adopt-blue-hover">Follow Us</a>
-              </div>
-            </div>
+      <div class="card custom-card-adopt">
+        <img src="images/adopt/sheep.png" alt="Sheep in a green paddock" class="card-img" />
+        <div class="card-img-overlay">
+          <div class="card-body custom-card-body-adopt">
+            <h4 class="card-title custom-card-title-adopt">Other Animals</h4>
+            <p class="card-text custom-card-text-adopt">Other animals that are up for adoption are posted on The Animal Sanctuary Facebook page as they become available.  Please follow us to see who needs a new home…</p>
+            <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="btn custom-btn-adopt-blue-hover">Follow Us</a>
           </div>
+        </div>
+      </div>
     </div>
   </div>
+  </div>
+  <!-- End Cards on xl, lg and md screens -->
+  <!-- Cards on xs and sm screens -->
+  <div class="row d-lg-none">
+    <div class="col-md-6 mb-3">
+      <div class="card custom-card-adopt h-100">
+        <img src=" images/adopt/hen.jpg" alt="A healthy adopted ex-egg farm hen" class="img-top" />
+        <div class="card-body">
+          <h4 class="card-title custom-card-title-adopt">Egg Farm Hens</h4>
+          <p class="card-text custom-card-text-adopt">The Animal Sanctuary works closely with other sanctuaries to rescue, rehabilitate and re-home young hens from commercial egg farms.</p>
+          <a href="#hen" class="btn custom-btn-adopt">Learn more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="card custom-card-adopt h-100">
+        <img src="images/adopt/sheep.png" alt="Sheep in a green paddock" class="card-img" />
+        <div class="card-body">
+          <h4 class="card-title custom-card-title-adopt">Other Animals</h4>
+          <p class="card-text custom-card-text-adopt">Other animals that are up for adoption are posted on The Animal Sanctuary Facebook page as they become available.  Please follow us to see who needs a new home…</p>
+          <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="btn custom-btn-adopt-blue-hover">Follow Us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Cards on xs and sm screens -->
+
 <hr/>
+  
+  <!-- Hen Content -->
   <div class="containter" id="hen">
     <h1>Egg Farm Hens</h1>
       <p>
@@ -60,6 +91,7 @@
         on Facebook.
     </p>
   </div>
+  <!-- End Hen Content-->
 
 </div>
-<!-- End Hens-->
+<!-- End Adopt-->
