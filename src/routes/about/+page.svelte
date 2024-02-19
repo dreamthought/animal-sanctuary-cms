0ee5797cdcea3967463b97a48440849e3d45fc5e
Byssa <name@email.com>
2024-02-19 23:08:48 +1300

    Updated ABOUT

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,26 +10,32 @@
     <img src="images/about/shawnandmichael.jpg" class="rounded" alt="Shawn Bishop and Michael Dixon">
   </div>
   <p class="custom-wrap-text">
-  <b>As a couple we have always been involved in rescuing injured birds and abused animals. When we lived in Auckland, 
-  we often worked with bird rescue groups to help transport natives and gave a loving home to a wide variety of animals.</b>
+  <b>As a couple we have always been involved in rescuing injured birds and abused animals. 
+    In 2002 we decided to take our passion a step further, and found the perfect site in the hills of Matakana. 
+    Our 32 acre property has grazing land to permanently home abused animals, and is bordered by 400 acres of protected bush where we can safely release rehabilitated native birds.  
+    When our family purchased the 60 acres next door, it meant we could expand even further.</b>
   </p>
   <p class="custom-wrap-text">
-  In 2002 we decided to take our passion a step further, and found the perfect site in the hills of Matakana. 
-  Our 32 acre property has grazing land to permanently home abused animals, and is bordered by 400 acres of protected bush 
-  where we can safely release rehabilitated native birds.
+    We have two of our original donkeys, Solomon and Bartholomew, featured in our header because they were our first rescues. 
+    Even before we had officially opened The Animal Sanctuary, we heard from a local donkey rescue group that a home was desperately needed. 
+    Three donkeys moved in the same day that we did, and lived their full lives here.  
+    They were the first of a long long line of rescued animals that have taken refuge here.
   </p>
   <p class="custom-wrap-text">
-  We have our two donkeys, Solomon and Bartholomew, on our header because they were our first rescues. 
-  Even before we had opened The Sanctuary we heard from a local donkey rescue group that a home was needed. 
-  They moved in the same day that we did, and have been with us ever since. When we were just getting started, we didn't plan on working with battery hens, 
-  but when we heard that they needed somewhere safe where they could rehabilitate and start over, we took them too. 
-  We now have about 100 animals in The Sanctuary: two-legged and four-legged, native and exotic, warm and cold-blooded - all getting the care that they deserve.
+    In the very early days, we didn't plan on working with battery hens.. 
+    but when we learned that a group of rescued hens urgently needed somewhere safe where they could rehabilitate and start over, we took them in, too.  
+    We have now rescued literally tens of thousands of hens.  
+    And the native bird work that we had started in Auckland became a key focus now that we had the perfect release site.  
   </p>
   <p class="custom-wrap-text">
-  We work long hours, and we fund the ongoing costs of food and veterinary care ourselves. On arrival our animals require intensive care and lots of TLC, 
-  but one of the most rewarding returns for all our hard work is watching a native bird soar away back to its natural life in the wild. 
-  Even our permanent residents are continually amazing us. Once they've overcome their fear - perceiving us at first as being just more humans to mistreat them
-   - they become gentle, trusting and caring. Animals can teach us a lot about forgiveness and being able to love and trust again.
+    We have between 100 - 300 animals at The Animal Sanctuary at any given time: two-legged and four-legged, native and exotic, warm and cold-blooded - 
+    all getting the care that they deserve.  We work long hours, and we fund the ongoing costs of food and veterinary care ourselves. 
+    We are 100% volunteer run, with no wages or salaries paid.  100% of donations go directly toward the animals.  
+    On arrival, animals and birds require intensive care and lots of TLC.  
+    There are many rewarding returns for all our hard work: watching a native bird soar away back to its natural life in the wild; 
+    an abused animal coming to us eagerly for a cuddle;  a sad animal find a new loving home with a wonderful family.  
+    Some of the most damaged animals stay here forever.  It amazes us that once they've overcome their fear, they become gentle, friendly and caring. 
+    Animals can teach us a lot about forgiveness and being able to love and trust again.
   </p>
   <img src="images/about/signoff.png" alt="Shawn and Michael Sign-off" class="custom-signoff" />
 </div>
