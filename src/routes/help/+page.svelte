8de78aa211c543c833cce4eef89f5ac1782e7ba3
Byssa <name@email.com>
2024-02-20 11:30:27 +1300

    Updated HELP. Added missing files.

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -7,39 +7,54 @@
 <div class="container custom-content shadow-lg">
     <h1>HOW YOU CAN HELP</h1>
       <div class="row">
-        <div class="col-md-3">
-          <div class="card custom-card-help">
-              <h4 class="card-header custom-card-title-animals">Give a Donation</h4>
+        <div class="col-lg-3 col-md-6 mb-3">
+          <div class="card custom-card-help h-100">
               <img src="images/help/donate.jpg" alt="Make a donation" class="card-img-top" />
-              <div class="card-img-overlay">
-                <a href="donate" class="btn custom-btn-animals">Learn more</a>
+              <div class="card-body">
+                <h4 class="card-title custom-card-title-help">Make a Donation</h4>
+                <p class="card-text custom-card-text-help">
+                  The Animal Sanctuary receives no government funding and is completely supported by donations to our Charitable Trust.
+                  If you'd like to support our work with injured native birds and abused animals, we'll very much appreciate your donation:</p>
+                <a href="donate" class="btn custom-btn-help">Learn more</a>
               </div>
             </div>
         </div>
-        <div class="col-md-3">
-          <div class="card custom-card-help">
-            <h4 class="card-header custom-card-title-animals">Become a Supporter</h4>
-            <img src="images/help/support.jpg" alt="Become a supporter" class="card-img-top" />
-            <div class="card-img-overlay">
-              <a href="support" class="btn custom-btn-animals">Learn more</a>
+        <div class="col-lg-3 col-md-6 mb-3">
+          <div class="card custom-card-help h-100">
+            <img src="images/help/trademe.jpg" alt="Donate Itmems to Op shop logo" class="card-img-top" />
+            <div class="card-body">
+              <h4 class="card-title custom-card-title-help">Donate Items to Op Shop</h4>
+              <p class="card-text custom-card-text-help">
+                Help us by donating a quality item to list on Trade Me for our “Animals Op Shop” store! 
+                The proceeds from this sale will help us continue the important work we do, and we couldn't do it without support from people like you.</p>
+              <a href="shop" class="btn custom-btn-help">Learn more</a>
             </div>
           </div>
         </div>
-        <div class="col-md-3">
-          <div class="card custom-card-help">
-              <h4 class="card-header custom-card-title-animals">Adopt a Hen</h4>
-              <img src="images/help/hen.jpg" alt="pigs" class="card-img-top" />
-              <div class="card-img-overlay">
-                <a href="adopt" class="btn custom-btn-animals">Learn more</a>
+        <div class="col-lg-3 col-md-6 mb-3">
+          <div class="card custom-card-help h-100">
+            <img src="images/help/support.jpg" alt="Become a Sanctuary Supporter logo" class="card-img-top" />
+              <div class="card-body">
+                <h4 class="card-title custom-card-title-help">Become a Sanctuary Supporter</h4>
+                <p class="card-title custom-card-text-help">
+                  Become a Sanctuary Supporter with an automatic donation of $5 or more per month. 
+                  You can visit The Animal Sanctuary (which is closed to the general public), meet the animals and birds, and see what we do.
+                </p>
+                <a href="support" class="btn custom-btn-help">Learn more</a>
               </div>
-            </div>
+          </div>
         </div>
-        <div class="col-md-3">
-          <div class="card custom-card-help">
-              <h4 class="card-header custom-card-title-animals">Save a Cow</h4>
-              <img src="images/help/cow.jpg" alt="parrots" class="card-img-top" />
-              <div class="card-img-overlay">
-                <a href="cowcare" class="btn custom-btn-animals">Learn more</a>
+        <div class="col-lg-3 col-md-6 mb-3">
+          <div class="card custom-card-help h-100">
+              <img src="images/help/cow.jpg" alt="Save a Cow - Cowcare logo" class="card-img-top" />
+              <div class="card-body">
+                <h4 class="card-title custom-card-title-help">Save a Cow</h4>
+                <p class="card-text custom-card-text-help">
+                  Rescued cows finally have an option. With your sponsorship they can go to Cow Care to live out their full life. 
+                  As a sponsor you can visit your cow; receive reports, photos & updates about your cow; join our private FB sponsors page; 
+                  receive invites to private Cow Care functions such as picnics, etc.
+                </p>
+                <a href="cowcare" class="btn custom-btn-help">Learn more</a>
               </div>
             </div>
         </div>
