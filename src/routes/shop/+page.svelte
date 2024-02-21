c51ecd9e3e3e0d96aa31968bb3a2738eedadfb3e
Byssa <name@email.com>
2024-02-22 11:34:40 +1300

    Added JotFORM on SHOP

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -47,80 +47,109 @@
     <!-- End row for shop options cards -->
 <hr/>
     <!-- Op Shop Donations -->
-    <div class="container custom-content-green text-center mt-3">
+    <div class="container text-center mt-3">
       <p class="fs-3 pt-2"><b>We welcome donations for our Animals Op Shop Store</b></p>
     </div>
+          <!-- Div for Form -->
     <div class="container mt-3">
-      <p>
-        <b><i>Thank you</i> for your generous offer to donate a quality item to list on Trade Me for our “Animals Op Shop” store!  
-        The proceeds from this sale will help us continue the important work we do, and we couldn’t do it without support from people like you.</b>
-      </p>
-      <p>
-        To help us create the best listing possible, please complete this form and send it back to us at <b class="custom-b-green">animalsopshop@gmail.com</b> with <b><i>at least three</i></b> photographs of the item. 
-        We suggest you follow these guidelines for photos:
-      </p>
-      <ul>
-        <li>Use high-resolution, or detailed photographs that clearly show the item details</li>
-        <li>Try and have the item be the only thing in the picture, or taking up as much of the picture as possible so its clear what item is for sale</li>
-        <li>Take your photos in bright or natural light and avoid shadows</li>
-        <li>Landscape (horizontal) photos work best</li>
-        <li>Photograph smaller items on a white, light background that’s free of distractions</li>
-        <li>Include photos from multiple angles, including closeups of any imperfections, brand names, or unique features</li>
-      </ul>
-      <!-- Div for Form -->
-      <div class="custom-form mt-2">
-        <form action="#">
-          <div class="form-group">
-            <label for="description">Description:</label>
-            <input type="description" class="form-control" id="description" placeholder="Please enter a detailed description of the item" name="description">
-            <small id="passwordHelpBlock" class="form-text text-muted">
-              Tips:
-            • Include size/measurements
-            • Note unique or special features that make this item stand out
-            • Include details or descriptions that you as a buyer would like to know about
-            • Note any imperfections or damage
-            </small>
-          </div>
-          <div class="form-group">
-            <label for="chickens">Condition of this item</label>
-          </div>
-          <select class="form-select" aria-label="">
-            <option selected>Please choose an option below</option>
-            <option value="Yes">New - brand new, original packaging</option>
-            <option value="No">Used - like new</option>
-            <option value="No">Used - very good</option>
-            <option value="No">Used - good</option>
-            <option value="No">Used - acceptable</option>
-          </select>
-          <div class="form-group">
-            <label for="condition">Retail or Current Value in $</label>
-            <input type="condition" class="form-control" id="condition" placeholder="What do you think it is worth?" name="condition">
-          </div>
-          <div class="form-group">
-            <label for="chickens">Shipping</label>
-          </div>
-          <select class="form-select" aria-label="">
-            <option selected>Select which options apply to your item</option>
-            <option value="Yes">I can post or courier the item (at buyer’s expense)</option>
-            <option value="No">The item can be picked up from (town)</option>
-          </select>
-          <div class="form-group">
-            <label for="condition">Your address for pickup IF allowed.  Will NOT be published in listing, only sent to buyer.</label>
-            <input type="condition" class="form-control" id="condition" placeholder="Please enter your address" name="condition">
-          </div>
-          <div class="form-group">
-            <label for="condition">Your phone number (if we need to call you): </label>
-            <input type="condition" class="form-control" id="condition" placeholder="Please enter your phone number" name="condition">
-          </div>
-          <div class="form-group">
-            <label for="condition">Your email address: </label>
-            <input type="condition" class="form-control" id="condition" placeholder="Please enter your email address" name="condition">
-          </div>
-          
 
-        <button type="submit" class="btn btn-success d-flex mx-auto mt-3">Send</button>
-      </form> 
-    </div>
+
+    <iframe
+    id="JotFormIFrame-240447944809062"
+    title="Animals Op Shop - Trade Me Donation Form"
+    onload="window.parent.scrollTo(0,0)"
+    allowtransparency="true"
+    allowfullscreen="true"
+    allow="geolocation; microphone; camera"
+    src="https://form.jotform.com/240447944809062"
+    frameborder="0"
+    style="min-width:100%;max-width:100%;height:539px;border:none;"
+    scrolling="no"
+  >
+  </iframe>
+  <script type="text/javascript">
+  var ifr = document.getElementById("JotFormIFrame-240447944809062");
+  if (ifr) {
+    var src = ifr.src;
+    var iframeParams = [];
+    if (window.location.href && window.location.href.indexOf("?") > -1) {
+      iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+    }
+    if (src && src.indexOf("?") > -1) {
+      iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+      src = src.substr(0, src.indexOf("?"))
+    }
+    iframeParams.push("isIframeEmbed=1");
+    ifr.src = src + "?" + iframeParams.join('&');
+  }
+  window.handleIFrameMessage = function(e) {
+    if (typeof e.data === 'object') { return; }
+    var args = e.data.split(":");
+    if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
+    if (!iframe) { return; }
+    switch (args[0]) {
+      case "scrollIntoView":
+        iframe.scrollIntoView();
+        break;
+      case "setHeight":
+        iframe.style.height = args[1] + "px";
+        if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
+          iframe.style.minHeight = args[1] + "px";
+        }
+        break;
+      case "collapseErrorPage":
+        if (iframe.clientHeight > window.innerHeight) {
+          iframe.style.height = window.innerHeight + "px";
+        }
+        break;
+      case "reloadPage":
+        window.location.reload();
+        break;
+      case "loadScript":
+        if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
+        var src = args[1];
+        if (args.length > 3) {
+            src = args[1] + ':' + args[2];
+        }
+        var script = document.createElement('script');
+        script.src = src;
+        script.type = 'text/javascript';
+        document.body.appendChild(script);
+        break;
+      case "exitFullscreen":
+        if      (window.document.exitFullscreen)        window.document.exitFullscreen();
+        else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
+        else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
+        else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
+        else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
+        break;
+    }
+    var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+    if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+      var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+      iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
+    }
+  };
+  window.isPermitted = function(originUrl, whitelisted_domains) {
+    var url = document.createElement('a');
+    url.href = originUrl;
+    var hostname = url.hostname;
+    var result = false;
+    if( typeof hostname !== 'undefined' ) {
+      whitelisted_domains.forEach(function(element) {
+          if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
+              result = true;
+          }
+      });
+      return result;
+    }
+  };
+  if (window.addEventListener) {
+    window.addEventListener("message", handleIFrameMessage, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onmessage", handleIFrameMessage);
+  }
+  </script>
     <!-- Form -->
     </div>
     <!-- End Op Shop Donations -->
