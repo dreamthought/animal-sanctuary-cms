470def019df7ebd36ff6d5e46dc526b2ab43fe51
Byssa <name@email.com>
2024-02-23 12:02:56 +1300

    Added NEWS

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,170 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>News - The Animal Sanctuary</title>
+</svelte:head>
+
+<!-- News -->
+<div class="container custom-content shadow-lg">
+  <h1>NEWS</h1>
+  <p class="text-center">
+    <b>Our NEWS page is updated from our Facebook page. To see more, come <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="in-text-link-facebook">Follow us on Facebook</a>.</b>
+  </p>
+  <!-- row for news items (single col on sm and below and lg and above, double col on md)-->
+  <div class="row row-cols-1">
+
+    <!-- news item -->
+    <div class="col">
+      <div class="row g-0 mb-3 custom-content-news">
+        <div class="col-lg-9 p-2">
+          <div class="d-flex">
+            <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
+            <!-- facebook post date-->
+            <small class="ms-auto">23 February</small>
+          </div>
+          <!-- facebook post text -->
+          <p>
+            FABULOUS FRIDAY
+            <br/>Work on the new roof for the pig's barn continues. Dan is determined to make as much progress as possible.  
+            It's hard to get him to stop for a coffee break! Once again, Alvaro's height is an advantage. 
+            Maureen and Elena are taking a pause after hauling a load of roofing iron and timer to the barn. All second hand, and wonderful!
+            <!-- facebook post text (hidden on lg) -->
+            <span class="d-md-inline d-lg-none d-xl-inline">
+            </span> 
+            <!-- link to facebook post -->
+            <br/><a href="https://www.facebook.com/TheAnimalSanctuaryNZ/posts/pfbid027FES2E4QxeGqJqge58AvZ5QTqBXErtYBS2EDf5ijc8CJDBTcPAHVyotme4xaTWSDl?__cft__[0]=AZW3G70HVbsm8qySFOC69iQ9NdzBQfoP1w-PsaAcR2LBQqQiKsvxxl7kV52h-Cwsf_nBlk8VUGj74-J_fI1kM_JDFiPzye-F6GshqmVitffrx2Wi6DSIRG1zXgISDdJ-DJb4IRcuMhmdmoTm5aFcgroj_APn6PR3aiJrTKsUDAl1AF48TZRG5oO5eQQTFTfWmBg&__tn__=%2CO%2CP-R" target="_blank" class="in-text-link-facebook">Continue reading <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-3 d-flex align-items-center align-content-center">
+          <!-- facebook post image (square ) -->
+          <img src="images/news/facebook-e.jpg" class="rounded img-fluid w-100" alt="Facebook">
+        </div>
+      </div>
+    </div>
+    <!-- end news item -->
+
+    <!-- news item 1 -->
+    <div class="col">
+      <div class="row g-0 mb-3 custom-content-news">
+        <div class="col-lg-9 p-2">
+          <div class="d-flex">
+            <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
+            <!-- facebook post date -->
+            <small class="ms-auto">22 February</small>
+          </div>
+          <!-- facebook post text -->
+          <p>
+            WHEN I HEARD STRANGE NOISES emanating from a bowl on top of the hutch, I just had to investigate...
+           <!-- facebook post text (hidden on lg) -->
+           <span class="d-md-inline d-lg-none d-xl-inline">
+           </span>
+            <!-- link to facebook post -->
+            <br/><a href="https://www.facebook.com/reel/1766443237192584/?s=single_unit&__cft__[0]=AZW125GaNhR0Vr8-VwVBAb3kRFimYNM_gvQO-wlAcp8VSljfZ7jth4EJ7LhKZON7VaDXUAEnW6jRfdn3oPx9GUnmcJyYPefELT7AzFyjtdPZQEw6FOBLzKEbAKDvblCj1Q7xjDLUk72A6H2andPdyM7ETRhBwTzMfeMQBlsD393RBjr_PbolktyJ-yhdhi0aVTY&__tn__=H-R" target="_blank" class="in-text-link-facebook">Continue reading <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-3 d-flex align-items-center align-content-center">
+          <!-- facebook post image (square)-->
+          <img src="images/news/facebook-a.png" class="rounded img-fluid w-100" alt="Facebook">
+        </div>
+      </div>
+    </div>
+    <!-- end news item 1 -->
+
+    <!-- news item 2 -->
+    <div class="col">
+      <div class="row g-0 mb-3 custom-content-news">
+        <div class="col-lg-9 p-2">
+          <div class="d-flex">
+            <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
+            <!-- facebook post date-->
+            <small class="ms-auto">21 February</small>
+          </div>
+          <!-- facebook post text -->
+          <p>
+            HEN RESCUE COMING UP
+            <br/>We are so happy to announce that there will be a BIG egg-farm hen rescue (1000 hens) on 22 March.  Yay!!  
+            The more hens that we have homes for, the more we can rescue.  So this is the time to apply to adopt some lucky 18 month old hens.
+             <!-- facebook post text (hidden on lg) -->
+             <span class="d-md-inline d-lg-none d-xl-inline">
+              <br/>All of our hen rescues are now coordinated by the wonderful Sally Ann of Franklin Farm Sanctuary.
+            <br/>So if you live in the regions of Auckland, Waikato, Tauranga, or Rotorua and would like to adopt, please apply using this li…
+            </span> 
+            <!-- link to facebook post -->
+            <br/><a href="https://www.facebook.com/TheAnimalSanctuaryNZ/posts/pfbid02465yd7jGFBMruu5GsiYGQjjUNC1S2nFqUvUybhpDfAzJFERUsT26dH2FpHPUVLKvl?__cft__[0]=AZW60zUzLLYEdzA1g5tGidz68nf5Zny_ev3hM9PwtIiGuX-w0JbJbOHF7tXBJLJ3JIANT2Vd5YPpks9casLdO2I0M5MB2vpKKNEapNl1c3AMjTvoRi4rCaqwmNmuLlOFll_m08wSux-V_Dhoc3MY4GnIRJB39TwyhiDhkfJ1EpOfjTo1ZCJmwLc0Mmy0acOE7UI&__tn__=%2CO%2CP-R" target="_blank" class="in-text-link-facebook">Continue reading <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-3 d-flex align-items-center align-content-center">
+          <!-- facebook post image (square ) -->
+          <img src="images/news/facebook-b.jpg" class="rounded img-fluid w-100" alt="Facebook">
+        </div>
+      </div>
+    </div>
+    <!-- end news item 2 -->
+
+    <!-- news item 3 -->
+    <div class="col">
+      <div class="row g-0 mb-3 custom-content-news">
+        <div class="col-lg-9 p-2">
+          <div class="d-flex">
+            <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
+            <!-- facebook post date -->
+            <small class="ms-auto">20 February</small>
+          </div>
+          <!-- facebook post text -->
+          <p>
+            GRAPHIC DESIGN VOLUNTEER 
+            <br/>UPDATE: Wow, we've already had 4 people express a willingness to help with this!  Incredible.  
+            I'm sure that something fabulous will result, so we should be all set now.  Thanks so much.
+            <!-- facebook post text (hidden on lg) -->
+            <span class="d-md-inline d-lg-none d-xl-inline">
+            <br/>-----------------------------------------
+            <br/>I'm bursting to announce a couple of new Animal Sanctuary projects that are being worked on, 
+            but know I can't say anything until they're ready to launch.  One is getting close to completion, and the other is in development.…
+            </span> 
+            <!-- link to facebook post -->
+            <br/><a href="https://www.facebook.com/TheAnimalSanctuaryNZ/posts/pfbid0mxhhLW4gbsXwEv4eGqg1zwLEqXJMFUnkNP2dPuzupBWzdWjCMBnqAQnpugp9AA1Zl?__cft__[0]=AZU7gpmOpU9kGJIn-SRQerjLxbQ4XdnVpPBRJ0cjDH-vcoDs2r7wRkyudgwVqGi8zYSGNnA7NFcITSY_fbjZJZr1lctnp-RVdOoB5WP4HfirInEaCqx6Ugs0IkDsdPtBRqaYpHwWnp4bDV1VOIqQmpkXdbbyKkZZLPKl90XxrQ0poZ64hs1nKPkLT1S5MnvaG7U&__tn__=%2CO%2CP-R" target="_blank" class="in-text-link-facebook">Continue reading <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-3 d-flex align-items-center align-content-center">
+          <!-- facebook post image (square) -->
+          <img src="images/news/facebook-c.jpg" class="rounded img-fluid w-100" alt="Facebook">
+        </div>
+      </div>
+    </div>
+    <!-- end news item 3 -->
+
+    <!-- news item 4 -->
+    <div class="col">
+      <div class="row g-0 mb-3 custom-content-news">
+        <div class="col-lg-9 p-2">
+          <div class="d-flex">
+            <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
+            <!-- facebook post date -->
+            <small class="ms-auto">20 February</small>
+          </div>
+          <!-- facebook post text -->
+          <p>
+            SAD NEWS, & NICE NEWS 
+            <br/>We received the very sad news that one of our wonderful Cow Care sponsors, Linda May, had passed away.  
+            We were fortunate to have connected with Linda through her love of cows, and are so sorry that she's gone too soon.  
+             <!-- facebook post text (hidden on lg) -->
+             <span class="d-md-inline d-lg-none d-xl-inline">
+              <br/>Linda's passing meant that her steer, the beautiful Bellamy, was without a sponsor.  
+              So we are extremely grateful that a lovely woman named Alex has stepped in to take over Bellamy's sponsorship, ensuring that he will continue to live a …
+            </span> 
+            <!-- link to facebook post -->
+            <br/><a href="https://www.facebook.com/TheAnimalSanctuaryNZ/posts/pfbid0XkPEVXghRo9xDR31c84QdnAXDMTZj8jpUYvNEJ1urrjGsJg8CXM83u1rfinFiHd3l?__cft__[0]=AZXloDwTajH-HhJxq_TcifWLyj5w_CEHqCe2efSEomBxxFY8n0Vra5-R7tESyuZdfsd_neoibAAY_u0NmXv0e8YT2lwG2VNR3LUhwuNKqqr9Moc7WVJbPsJmt3DKVP_OqUy3OoyIKQ2pKldCYBMyOq55gQNEKvQXFlQu0z8zOzlCuJSl65i2Ng0FGFOZabEmCg4&__tn__=%2CO%2CP-R" target="_blank" class="in-text-link-facebook">Continue reading <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-3 d-flex align-items-center align-content-center">
+          <!-- facebook post image (square) -->
+          <img src="images/news/facebook-d.jpg" class="rounded img-fluid w-100" alt="Facebook">
+        </div>
+      </div>
+    </div>
+    <!-- end news item 4 -->
+    
+  </div> 
+  <!-- end row for news items -->
+
+</div>  
+<!-- End News -->
