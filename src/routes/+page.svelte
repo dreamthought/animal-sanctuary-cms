e8d97684fd953376094488267079b1a5f039f310
Byssa <name@email.com>
2024-03-11 00:00:28 +1300

    Changed Vimeo video to custom iframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,13 @@
       <div class="row">
         <div class="col-xl-6 mb-3">
           <!-- Video -->
-          <div style="padding:56.25% 0 0 0;position:relative;">
-            <iframe src="https://player.vimeo.com/video/921637852?h=3570e547eb&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen class="rounded"></iframe>
-          </div><script src="https://player.vimeo.com/api/player.js"></script>
+          <div class="container-iframe">
+            <iframe class="responsive-iframe rounded"
+                    src="https://player.vimeo.com/video/921637852?h=3570e547eb&title=0&byline=0&portrait=0" 
+                    allow="fullscreen"
+                    title="The Animal Sanctuary Vimeo promo video">
+            </iframe>
+          </div>
           <!-- End Video -->
         </div>
         <div class="col-xl-6">
