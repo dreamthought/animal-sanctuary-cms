c5f911f13f564d99b2aacd12ba38f50e710966b5
Byssa <name@email.com>
2024-03-11 22:35:28 +1300

    Re-arranged home cards. Tidied up html.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,112 +3,113 @@
 <title>The Animal Sanctuary</title>
 </svelte:head>
 
-<script>
-</script>
-
-
-    <!-- Home -->
-    <div class="container custom-content shadow-lg">
-      <h1>WELCOME TO THE ANIMAL SANCTUARY</h1>
-      <div class="row">
-        <div class="col-xl-6 mb-3">
-          <!-- Video -->
-          <div class="container-iframe">
-            <iframe class="responsive-iframe rounded"
-                    src="https://player.vimeo.com/video/921637852?h=3570e547eb&title=0&byline=0&portrait=0" 
-                    allow="fullscreen"
-                    title="The Animal Sanctuary Vimeo promo video">
-            </iframe>
-          </div>
-          <!-- End Video -->
-        </div>
-        <div class="col-xl-6">
-          <p>
-          <b>The Animal Sanctuary is run by Shawn Bishop and Michael Dixon, and is based in Matakana, New Zealand.
-          We provide a safe haven for abused and neglected animals, and care for injured and orphaned native birds.</b>
-          </p>
-          <p>
-            Our 32 acre property has paddocks and barns to care for abused farm animals. 
-            Some stay forever, and others are rehomed when they recover. 
-            We're bordered by 400 acres of protected bush where we can safely release rehabilitated native birds.
-          </p>
-          <p>
-            We often care for between 100 – 300 animals at The Animal Sanctuary: two-legged and
-            four-legged, native and exotic, warm and cold-blooded – all getting the care that they deserve.
-          </p>
-          <p class="text-center">
-          <b>You can <a href="https://www.youtube.com/watch?v=13F_Em_z83E" target="_blank" class="in-text-link">watch our video</a>
-          to learn more about what we do, and <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="in-text-link-facebook">Follow us on Facebook</a> to see what we get up to.</b>
-          </p>
-        </div>
+<!-- Home -->
+<div class="container custom-content shadow-lg">
+  <h1>WELCOME TO THE ANIMAL SANCTUARY</h1>
+  <div class="row">
+    <div class="col-xl-6 mb-3">
+      <!-- Video -->
+      <div class="container-iframe">
+        <iframe class="responsive-iframe rounded"
+                src="https://player.vimeo.com/video/921637852?h=3570e547eb&title=0&byline=0&portrait=0" 
+                allow="fullscreen"
+                title="The Animal Sanctuary Vimeo promo video">
+        </iframe>
       </div>
-<hr/>
-      <!-- What We Do -->
-      <div class="row">
-        <h3 class="mb-3">The Animal Sanctuary is a haven for a wide range of animals with a focus on three main areas of work:</h3>
-        <div class="col-md-4">
-            <div class="card custom-card-home">
-                <img src=" images/home/nativebirds.jpg" alt="Two native wood pigeons - kereru" class="card-img" />
-                <div class="card-img-overlay">
-                  <div class="card-body custom-card-body-home">
-                    <h4 class="card-title custom-card-title-home">Native Birds</h4>
-                    <p class="card-text custom-card-text-home">We rehabilitate and release native New Zealand birds back into the wild.</p>
-                    <a href="nativebirds" class="btn custom-btn-home">Learn more</a>
-                  </div>
-                </div>
-              </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card custom-card-home">
-                <img src="images/home/rescueanimals.jpg" alt="Young rescue goat" class="card-img" />
-                <div class="card-img-overlay">
-                  <div class="card-body custom-card-body-home">
-                    <h4 class="card-title custom-card-title-home">Rescued Animals</h4>
-                    <p class="card-text custom-card-text-home">The Animal Sanctuary is a permanent home and refuge for abandoned, abused, and forgotten animals.</p>
-                    <a href="animals" class="btn custom-btn-home">Learn more</a>
-                  </div>
-                </div>
-              </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card custom-card-home">
-                <img src="images/home/batteryhens.jpg" class="card-img" alt="Ex Egg Farm Hen" />
-                <div class="card-img-overlay">
-                  <div class="card-body custom-card-body-home">
-                    <h4 class="card-title custom-card-title-home">Egg-Farm Hens</h4>
-                    <p class="card-text custom-card-text-home">We rehabilitate & rehome hens from commercial egg farms.</p>
-                    <a href="adopt" class="btn custom-btn-home">Learn more</a>
-                  </div>
-                </div>
-              </div>
-        </div>
-
-      </div>
-      <!-- End What We Do -->
-<hr/>
-      <!-- Sponsorship request  -->
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 g-0">
-            <img src="images/home/coffeeimage.jpeg" class="img-fluid" alt="The Sanctuary donkey and owl sitting at a table sharing coffee">
-          </div>
-
-          <div class="col-lg-6 pt-5 custom-content-coffee">
-              <p>
-                <b>For the price of just one coffee a month, you can provide real help to injured and orphaned native birds and rescued abused animals.</b>
-              </p>
-              <p>
-                Become a Sanctuary Supporter with an automatic donation of $5 or more per month. 
-                You can visit The Animal Sanctuary (which is closed to the general public), meet the animals and birds, and see what we do. 
-                And because we are a registered charity, you can even receive one-third of your donation back each year as a tax refund! 
-              </p>
-              <p class="text-center">
-                <a href="support" class="btn custom-btn-home p-3 mt-2 fs-5"><b>Yes, I want to become a supporter!</b></a>
-              </p>
-          </div>
-        </div>
-      </div>
-      <!-- End Sponsorship request -->
-    
+      <!-- End Video -->
     </div>
-    <!-- End Home -->
+    <!-- Intro -->
+    <div class="col-xl-6">
+      <p>
+        <b>The Animal Sanctuary is run by Shawn Bishop and Michael Dixon, and is based in Matakana, New Zealand.
+        We provide a safe haven for abused and neglected animals, and care for injured and orphaned native birds.</b>
+      </p>
+      <p>
+        Our 32 acre property has paddocks and barns to care for abused farm animals. 
+        Some stay forever, and others are rehomed when they recover. 
+        We're bordered by 400 acres of protected bush where we can safely release rehabilitated native birds.
+      </p>
+      <p>
+        We often care for between 100 – 300 animals at The Animal Sanctuary: two-legged and
+        four-legged, native and exotic, warm and cold-blooded – all getting the care that they deserve.
+      </p>
+      <p class="text-center">
+        <b>You can <a href="https://www.youtube.com/watch?v=13F_Em_z83E" target="_blank" class="in-text-link">watch our video</a>
+        to learn more about what we do, 
+        and <a href="https://www.facebook.com/TheAnimalSanctuaryNZ" target="_blank" class="in-text-link-facebook">Follow us on Facebook</a> to see what we get up to.</b>
+      </p>
+    </div>
+  </div>
+  <!-- End Intro -->
+<hr/>
+  <!-- What We Do -->
+  <div class="row">
+    <h3 class="mb-3">The Animal Sanctuary is a haven for a wide range of animals with a focus on three main areas of work:</h3>
+
+    <div class="col-md-4">
+      <div class="card custom-card-home">
+        <img src="images/home/rescueanimals.jpg" alt="Young rescue goat" class="card-img" />
+        <div class="card-img-overlay">
+          <div class="card-body custom-card-body-home">
+            <h4 class="card-title custom-card-title-home">Rescued Animals</h4>
+            <p class="card-text custom-card-text-home">The Animal Sanctuary is a permanent home and refuge for abandoned, abused, and forgotten animals.</p>
+            <a href="animals" class="btn custom-btn-home">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card custom-card-home">
+        <img src="images/home/batteryhens.jpg" class="card-img" alt="Ex Egg Farm Hen" />
+        <div class="card-img-overlay">
+          <div class="card-body custom-card-body-home">
+            <h4 class="card-title custom-card-title-home">Egg-Farm Hens</h4>
+            <p class="card-text custom-card-text-home">We rehabilitate & rehome hens from commercial egg farms.</p>
+            <a href="adopt" class="btn custom-btn-home">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card custom-card-home">
+        <img src=" images/home/nativebirds.jpg" alt="Two native wood pigeons - kereru" class="card-img" />
+        <div class="card-img-overlay">
+          <div class="card-body custom-card-body-home">
+            <h4 class="card-title custom-card-title-home">Native Birds</h4>
+            <p class="card-text custom-card-text-home">We rehabilitate and release native New Zealand birds back into the wild.</p>
+            <a href="nativebirds" class="btn custom-btn-home">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!-- End What We Do -->
+<hr/>
+  <!-- Sponsorship request  -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 g-0">
+        <img src="images/home/coffeeimage.jpeg" class="img-fluid" alt="The Sanctuary donkey and owl sitting at a table sharing coffee">
+      </div>
+      <div class="col-lg-6 pt-5 custom-content-coffee">
+        <p>
+          <b>For the price of just one coffee a month, you can provide real help to injured and orphaned native birds and rescued abused animals.</b>
+        </p>
+        <p>
+          Become a Sanctuary Supporter with an automatic donation of $5 or more per month. 
+          You can visit The Animal Sanctuary (which is closed to the general public), meet the animals and birds, and see what we do. 
+          And because we are a registered charity, you can even receive one-third of your donation back each year as a tax refund! 
+        </p>
+        <p class="text-center">
+          <a href="support" class="btn custom-btn-home p-3 mt-2 fs-5"><b>Yes, I want to become a supporter!</b></a>
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- End Sponsorship request -->
+    
+</div>
+<!-- End Home -->
