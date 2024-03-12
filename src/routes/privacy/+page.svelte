2dce0b0e3b29333e7a79ac9999f821f9f6fb356c
Byssa <name@email.com>
2024-03-12 15:59:47 +1300

    Added Privacy Policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,56 @@
+<!-- Add headers here -->
+<svelte:head>
+<title>The Animal Sanctuary - Privacy Policy</title>
+</svelte:head>
+
+<!-- Privacy Policy -->
+<div class="container custom-content shadow-lg">
+  <h1>Privacy Policy</h1>
+  <div class="container">
+    <p>
+      At The Animal Sanctuary we understand that you value your privacy and wish to keep your personal information secure. 
+      This privacy policy document outlines how and when we collect personal information and how it is used.
+    </p>
+    <p>
+      <b><u>Information we collect and why we collect it</u></b>
+      <br/><b>User-provided Information</b>
+      <br/><i>You can use this website without disclosing any personal information.</i>
+      <br/>When you fill in any forms on our website, the personal information provided will be collected by us and can be viewed by us, designated volunteers and our website administrator.
+      If you email or contact us on Facebook Messenger, we may keep the content of your messages and our responses and hold your contact details. 
+      We will use your information to respond to you regarding the reason you contacted us. 
+      We will not share your information with any third party other than for legal reasons.
+      <br/><b>Information collected automatically</b>
+      <br/>We may collect statistical information about your visit. 
+      <i>This information is aggregated and doesn't identify you personally.</i>
+      The statistical information can be viewed by us and our website provider and can collect information such as your location, your activities on our website,
+      and information about your device. It can be used to analyze visitor trends to help improve our website via our web analytics tool Google Analytics. 
+      For more information on how Google collects and processes your data, visit <a href="https://www.google.com/policies/privacy/partners/" target="_blank" style="text-decoration:none;">www.google.com/policies/privacy/partners</a>.
+      You can prevent Google Analytics from using your information by downloading the <a href="https://tools.google.com/dlpage/gaoptout" target="_blank" style="text-decoration:none">Google Analytics opt-out browser add-on</a>.
+    </p>
+    <p>
+      <b><u>Embedded content from other websites</u></b>
+      <br/>Pages on our website may include embedded content (e.g. videos). 
+      Embedded content from other websites behaves in the exact same way as you if have visited the other website.
+    </p>
+    <p>
+      <b><u>Security, retaning and deleting your personal date</u></b>
+      <br/>When you submit your information via our website’s form or email, we will take all reasonable precautions to protect your information.
+      We retain the information we collect from you only for as long as we need it for it's intended purpose. 
+      You can request access to any personal data we hold about you to correct or delete it.
+    </p>
+    <p>
+      <b><u>Links to other websites</u></b>
+      <br/>We may have included links on this website for your use and reference. We are not responsible for the privacy policies or the content of these websites.
+    </p>
+    <p>
+      <b><u>Updates</u></b>
+      <br/>Our privacy policy may change from time to time, and all updates will be posted on this page.
+    </p>
+    <p>
+      <b><u>Contact Us</u></b>
+      <br/>If you have questions or concerns about our privacy policy please <a href="contact" class="in-text-link">contact us</a>.
+    </p>
+  
+    </div>
+  </div>
+  <!-- End Privacy Policy -->
