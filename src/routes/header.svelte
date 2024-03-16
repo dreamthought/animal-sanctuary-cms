2b84415462b4171537705b065105ce6d01530c7b
Raf Gemmail <rafiq@dreamthought.com>
2024-03-16 07:47:18 +0000

    Header nav collapse hack

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,12 +4,21 @@
       <img src="images/sanctuary-banner.png" alt="Animal Sanctuary Logo" class="img-fluid" /></a>
   </header>
   <!-- End Header -->
+  
+  <script>
+    let navButton;
+    const show_hide = () => {
+      if (! navButton.classList.contains("collapsed")){
+        navButton.click();
+      }
+    }
+  </script>
 
   <!-- Navigation -->
   <nav class="sticky-top navbar navbar-expand-xl custom-navbar">
     <!-- svelte-ignore a11y-missing-content a11y-missing-attribute -->
     <a class="d-lg-node d-xl-none navbar-brand" href="."></a>
-    <button class="navbar-toggler custom-toggler shadow-none" type="button" data-bs-toggle="collapse"
+    <button bind:this={navButton} class="navbar-toggler custom-toggler shadow-none collapsed" type="button" data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
       aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -17,25 +26,25 @@
     <div class="justify-content-center navbar-collapse collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item px-3">
-          <a class="nav-link" href=".">HOME</a>
+          <a on:click={show_hide} class="nav-link" href=".">HOME</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="about">ABOUT</a>
+          <a on:click={show_hide} class="nav-link" href="about">ABOUT</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="adopt">ADOPT</a>
+          <a on:click={show_hide} class="nav-link" href="adopt">ADOPT</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="shop">SHOP</a>
+          <a on:click={show_hide} class="nav-link" href="shop">SHOP</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="help">DONATE/HELP</a>
+          <a on:click={show_hide} class="nav-link" href="help">DONATE/HELP</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="news">NEWS</a>
+          <a on:click={show_hide} class="nav-link" href="news">NEWS</a>
         </li>
         <li class="nav-item px-3">
-          <a class="nav-link" href="contact">CONTACT</a>
+          <a on:click={show_hide} class="nav-link" href="contact">CONTACT</a>
         </li>
       </ul>
     </div>
