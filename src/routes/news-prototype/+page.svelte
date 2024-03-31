c802216703b99f323423ce81acacf061264f0798
Raf Gemmail <rafiq@dreamthought.com>
2024-03-31 20:09:09 +0000

    Restyled for desktop

--- src/routes/news-prototype/+page.svelte
+++ src/routes/news-prototype/+page.svelte
@@ -2,14 +2,19 @@
 <svelte:head>
 <title>News | The Animal Sanctuary</title>
 </svelte:head>
-
-
 <script>
   import { onMount } from 'svelte'
+  const callToAction = "Some exciting news from the Animal Sanctuary!"
+  const postLimit = 75;
   const apiEndpoint = 'https://560dcon6p6.execute-api.us-east-1.amazonaws.com/default/animal-sanctuary-news-api-poc'
   let feed = [];
-  const deNewline= (text) => text.split("\n")
-
+  //const deNewline= (text) => (text.substring(0,128)+"...").split("\n")
+  //const deNewline= (text) => (text.substring(0,postLimit)+"...").split("\n")
+  //const deNewline= (text) => [text]
+  //const deNewline= (text) => text.split("\n")
+  const deNewline= (text) => !text ? [callToAction] : (text.split(" ").slice(0, postLimit).join(" ") + " ...").split("\n")
+  //const deNewline = (text) => [text.substring(0,100) + "..."]
+  const formatDate = (date) => (new Date(date)).toDateString()
   const fetchData = async () => { 
     const response = await fetch(apiEndpoint, {
       method: 'GET',
@@ -26,6 +31,27 @@
   onMount(fetchData);
 </script>
 
+<style>
+.news-container {
+  max-height: 25rem;
+}
+
+.news-img-container {
+  text-align: right;
+  background: #350b06;
+  -webkit-clip-path: ellipse(60% 65% at 70% 50%);
+  clip-path: ellipse(60% 65% at 70% 50%);
+}
+
+.news-img-crop {
+  object-fit: scale-down;
+  object-position: right;
+  max-height: 20rem;
+  margin: 0 0 0 auto;
+}
+</style>
+
+
 <div class="container custom-content shadow-lg">
   <h1>NEWS</h1>
   <p class="text-center">
@@ -37,26 +63,26 @@
   {#each feed as post}
 <!-- News -->
     <!-- news item  -->
-    <div class="col">
+    <div class="col news-container">
       <div class="row g-0 mb-3 custom-content-news">
         <div class="col-lg-9 p-2">
           <div class="d-flex">
             <img src="images/news/facebook-logo.png" alt="The Animal Sanctuary NZ Facebook logo" class="custom-news-logo" /><b>The Animal Sanctuary</b>
             <!-- facebook post date-->
-            <small class="ms-auto">{post.created_time}</small>
+            <small class="ms-auto">{formatDate(post.created_time)}</small>
           </div>
           <!-- facebook post text -->
           <p>
             {#each deNewline(post.message) as paragraph}
-              <p>{paragraph}</p>
+            {paragraph}<br/>
             {/each}
             <!-- link to facebook post -->
             <br/><a href="{post.permalink_url}" target="_blank" class="in-text-link-facebook">See more <i class="fa-solid fa-arrow-right"></i></a>
           </p>
         </div>
-        <div class="col-lg-3 d-flex align-items-center align-content-center">
+        <div class="col-lg-3 d-flex align-items-center align-content-center news-img-container">
           <!-- facebook post image (square ) -->
-          <img src="{post.full_picture}" class="rounded img-fluid w-100" alt="Facebook">
+          <img src="{post.full_picture}" class="news-crop rounded img-fluid w-100 news-img-crop" alt="Facebook">
         </div>
       </div>
     </div>
