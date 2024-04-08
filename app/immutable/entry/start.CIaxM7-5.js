5fe834bb637933395da8eeb362043359901f8ca0
Byssa <name@email.com>
2024-04-09 11:28:53 +1200

    Updates

---
+++ app/immutable/entry/start.CIaxM7-5.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.3wYeboPp.js";export{t as start};
---
+++ app/immutable/nodes/3.CneJ5TFn.js
@@ -0,0 +1,17 @@
+import{s as l,n as s}from"../chunks/scheduler.h1jTRUOO.js";import{S as h,i as c,s as u,e as m,h as f,d as r,c as w,a as g,g as p,b as v,f as i}from"../chunks/index.ZqXQ7sVt.js";function y(d){let t,e,o=`<h1>ABOUT US</h1> <div class="custom-wrap-img text-center"><img src="images/about/shawnandmichael.jpg" class="rounded" alt="Shawn Bishop and Michael Dixon"/></div> <p class="custom-wrap-text"><b>As a couple we have always been involved in rescuing injured birds and abused animals. 
+    In 2002 we decided to take our passion a step further, and found the perfect site in the hills of Matakana. 
+    Our 32 acre property has grazing land to permanently home abused animals, and is bordered by 400 acres of protected bush where we can safely release rehabilitated native birds.  
+    When our family purchased the 60 acres next door, it meant we could expand even further.</b></p> <p class="custom-wrap-text">We have two of our original donkeys, Solomon and Bartholomew, featured in our header because they were our first rescues. 
+    Even before we had officially opened The Animal Sanctuary, we heard from a local donkey rescue group that a home was desperately needed. 
+    Three donkeys moved in the same day that we did, and lived their full lives here.  
+    They were the first of a long long line of rescued animals that have taken refuge here.</p> <p class="custom-wrap-text">In the very early days, we didn&#39;t plan on working with battery hens.. 
+    but when we learned that a group of rescued hens urgently needed somewhere safe where they could rehabilitate and start over, we took them in, too.  
+    We have now rescued literally tens of thousands of hens.  
+    And the native bird work that we had started in Auckland became a key focus now that we had the perfect release site.</p> <p class="custom-wrap-text">We have between 100 - 300 animals at The Animal Sanctuary at any given time: two-legged and four-legged, native and exotic, warm and cold-blooded - 
+    all getting the care that they deserve.  We work long hours, and we fund the ongoing costs of food and veterinary care ourselves. 
+    We are 100% volunteer run, with no wages or salaries paid.  100% of donations go directly toward the animals.  
+    On arrival, animals and birds require intensive care and lots of TLC.  
+    There are many rewarding returns for all our hard work: watching a native bird soar away back to its natural life in the wild; 
+    an abused animal coming to us eagerly for a cuddle;  a sad animal find a new loving home with a wonderful family.  
+    Some of the most damaged animals stay here forever.  It amazes us that once they&#39;ve overcome their fear, they become gentle, friendly and caring. 
+    Animals can teach us a lot about forgiveness and being able to love and trust again.</p> <img src="images/about/signoff.png" alt="Shawn and Michael Sign-off" class="custom-signoff"/>`;return{c(){t=u(),e=m("div"),e.innerHTML=o,this.h()},l(a){f("svelte-rz27r7",document.head).forEach(r),t=w(a),e=g(a,"DIV",{class:!0,"data-svelte-h":!0}),p(e)!=="svelte-t3q4q"&&(e.innerHTML=o),this.h()},h(){document.title="About Us | The Animal Sanctuary",v(e,"class","container custom-content shadow-lg")},m(a,n){i(a,t,n),i(a,e,n)},p:s,i:s,o:s,d(a){a&&(r(t),r(e))}}}class x extends h{constructor(t){super(),c(this,t,null,y,l,{})}}export{x as component};
