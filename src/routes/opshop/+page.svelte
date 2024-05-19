67faa15ebdc7ba8c38cddd0ec7a3b206de2566ba
Byssa <name@email.com>
2024-05-19 23:44:23 +1200

    Changed Opshop Jotform

--- src/routes/opshop/+page.svelte
+++ src/routes/opshop/+page.svelte
@@ -16,20 +16,19 @@
 <hr/>
 
     <!-- JotForm -->
-    <iframe
-      id="JotFormIFrame-240987308713059"
-      title="The Animal's Online Op Shop"
-      onload="window.parent.scrollTo(0,0)"
-      allowtransparency="true"
-      allow="geolocation; microphone; camera; fullscreen"
-      src="https://form.jotform.com/240987308713059"
-      frameborder="0"
-      style="min-width:100%;max-width:100%;height:539px;border:none;"
-      scrolling="yes"
-    >
-    </iframe>
-    <script src='https://form.jotform.com/s/umd/latest/for-form-embed-handler.js'></script>
-    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-240987308713059'", "https://form.jotform.com/")</script>
+    <iframe 
+    id="JotFormIFrame-241393060625856" 
+    title="The Animals Online Op Shop" 
+    onload="window.parent.scrollTo(0,0)" 
+    allowtransparency="true" 
+    allow="geolocation; microphone; camera; fullscreen" 
+    src="https://form.jotform.com/241393060625856" 
+    frameborder="0" 
+    style="min-width:100%;max-width:100%;height:539px;border:none;" 
+    scrolling="yes" > 
+    </iframe> 
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script> 
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-241393060625856']", "https://form.jotform.com/")</script> 
     <!-- End JotForm -->
 
   </div>
